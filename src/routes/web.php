b34c84a7325407b2d843f04c1013d59a3a429814
maruyu <goldenleaf3310@gmail.com>
2025-06-11 17:08:18 +0900

    ルートに/admin追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,5 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/admin', function () {
-    return view('admin');
+    return view('admin.index');
 })->middleware('auth');
