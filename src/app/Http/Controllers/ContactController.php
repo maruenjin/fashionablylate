104276e43ebb76f07cb4911961069471f0285150
maruyu <goldenleaf3310@gmail.com>
2025-06-12 12:48:55 +0900

    confirmとthanks追加

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('contact.index');
+    }
+    public function confirm()
+    {
+        return '確認画面';
+    }
+
+    public function thanks()
+    {
+        return 'ありがとう画面';
+    }
 }
