6d78b26dc9c06c9fa06338c3d2489480897555ec
maruyu <goldenleaf3310@gmail.com>
2025-06-13 14:42:27 +0900

    エラーメッセージ記述

--- src/app/Http/Requests/ContactRequest.php
+++ src/app/Http/Requests/ContactRequest.php
@@ -36,4 +36,25 @@ class ContactRequest extends FormRequest
                 'content' => ['required', 'max:120'],   
         ];
     }
+    public function messages()
+{
+    return [
+       'last_name.required' => '姓を入力してください',
+        'first_name.required' => '名を入力してください',
+        'gender.required' => '性別を選択してください',
+        'email.required' => 'メールアドレスを入力してください',
+        'email.email' => 'メールアドレスはメール形式で入力してください',
+        'tel1.required' => '電話番号を入力してください',
+        'tel1.digits_between' => '電話番号は5桁までの数字で入力してください',
+        'tel2.required' => '電話番号を入力してください',
+        'tel2.digits_between' => '電話番号は5桁までの数字で入力してください',
+        'tel3.required' => '電話番号を入力してください',
+        'tel3.digits_between' => '電話番号は5桁までの数字で入力してください',
+        'address.required' => '住所を入力してください',
+        'category_id.required' => 'お問い合わせの種類を選択してください',
+        'content.required' => 'お問い合わせ内容を入力してください',
+        'content.max' => 'お問い合わせ内容は120文字以内で入力してください',
+    ];
+}
+
 }
