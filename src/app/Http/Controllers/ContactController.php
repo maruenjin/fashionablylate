53db371f1772301cf18654eacc025377192b59fd
maruyu <goldenleaf3310@gmail.com>
2025-06-13 23:00:16 +0900

    バリデーション表記追加

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -3,16 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ContactRequest;
 
 class ContactController extends Controller
 {
     public function index()
     {
-        return view('contact.index');
+        $categories = [
+            1 => '商品のお届けについて',
+            2 => '商品の交換について',
+            3 => '商品トラブル',
+            4 => 'ショップへのお問い合わせ',
+            5 => 'その他'
+        ];
+        return view('contact.index',compact('categories'));
     }
-    public function confirm()
+    public function confirm(ContactRequest $request)
     {
-        return '確認画面';
+        $inputs = $request->all();
+        return view('contact.confirm',compact('inputs'));
     }
 
     public function thanks()
