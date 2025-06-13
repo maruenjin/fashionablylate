38ae4aa60a35958bcb3098278d8c60b0994147cd
maruyu <goldenleaf3310@gmail.com>
2025-06-14 00:37:58 +0900

    ContactFactory変更

--- src/database/factories/ContactFactory.php
+++ src/database/factories/ContactFactory.php
@@ -14,7 +14,15 @@ class ContactFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'last_name' => $this->faker->lastName,
+            'first_name' => $this->faker->firstName,
+            'gender' => $this->faker->randomElement([1, 2, 3]),
+            'email' => $this->faker->email,
+            'tel' => $this->faker->numerify('###-####-####'),
+            'address' => $this->faker->address,
+            'building' => $this->faker->secondaryAddress,
+            'category_id' => $this->faker->numberBetween(1, 5),
+            'detail' => $this->faker->sentence(1),
         ];
     }
 }
