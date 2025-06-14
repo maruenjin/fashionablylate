9ca9d4439ab35f41e8eb4718189767d77c8aab16
maruyu <goldenleaf3310@gmail.com>
2025-06-14 22:08:41 +0900

    index.blade作成

--- src/resources/views/contact/index.blade.php
+++ src/resources/views/contact/index.blade.php
@@ -1,52 +1,66 @@
-@extends('layouts.app')
-
-@section('title', 'FashionablyLate')
-
-@section('css')
-    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
-@endsection
-
+@extends('layouts.app') 
 @section('content')
-    <div class="auth-container">
-        <h2 class="auth-title">Register</h2>
+<div class="container">
+    <h1>FashionablyLate</h1>
+    <h2>Contact</h2>
 
-        @if ($errors->any())
-            <div class="error-box">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+    @if ($errors->any())
+        <div style="color: red;">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
+    <form action="{{ route('contact.confirm') }}" method="POST">
+        @csrf
 
-            <div class="form-group">
-                <label for="name">お名前</label>
-                <input type="text" name="name" id="name" placeholder="例：山田 太郎" value="{{ old('name') }}">
-            </div>
+        {{-- お名前 --}}
+        <label>お名前 ※</label><br>
+        <input type="text" name="last_name" placeholder="例: 山田" value="{{ old('last_name') }}">
+        <input type="text" name="first_name" placeholder="例: 太郎" value="{{ old('first_name') }}"><br><br>
 
-            <div class="form-group">
-                <label for="email">メールアドレス</label>
-                <input type="email" name="email" id="email" placeholder="例：test@example.com" value="{{ old('email') }}">
-            </div>
+        {{-- 性別 --}}
+        <label>性別 ※</label><br>
+        <input type="radio" name="gender" value="1" {{ old('gender', '1') == '1' ? 'checked' : '' }}> 男性
+        <input type="radio" name="gender" value="2" {{ old('gender') == '2' ? 'checked' : '' }}> 女性
+        <input type="radio" name="gender" value="3" {{ old('gender') == '3' ? 'checked' : '' }}> その他<br><br>
 
-            <div class="form-group">
-                <label for="password">パスワード</label>
-                <input type="password" name="password" id="password" placeholder="例：coachtech1106">
-            </div>
+        {{-- メール --}}
+        <label>メールアドレス ※</label><br>
+        <input type="email" name="email" placeholder="例: test@example.com" value="{{ old('email') }}"><br><br>
 
-            <div class="form-group">
-                <label for="password_confirmation">パスワード（確認）</label>
-                <input type="password" name="password_confirmation" id="password_confirmation">
-            </div>
+        {{-- 電話番号 --}}
+        <label>電話番号 ※</label><br>
+        <input type="text" name="tel1" size="5" value="{{ old('tel1') }}"> -
+        <input type="text" name="tel2" size="5" value="{{ old('tel2') }}"> -
+        <input type="text" name="tel3" size="5" value="{{ old('tel3') }}"><br><br>
 
-            <div class="form-group">
-                <button type="submit">登録</button>
-            </div>
-        </form>
-    </div>
+        {{-- 住所 --}}
+        <label>住所 ※</label><br>
+        <input type="text" name="address" placeholder="例: 東京都新宿区下落合3-9-3" value="{{ old('address') }}"><br><br>
+
+        {{-- 建物名 --}}
+        <label>建物名</label><br>
+        <input type="text" name="building" placeholder="例: 早稲田マンション101" value="{{ old('building') }}"><br><br>
+
+        {{-- お問い合わせの種類 --}}
+        <label>お問い合わせの種類 ※</label><br>
+        <select name="category_id">
+            <option value="">選択してください</option>
+            @foreach ($categories as $id => $content)
+                <option value="{{ $id }}" {{ old('category_id') == $id ? 'selected' : '' }}>{{ $content }}</option>
+            @endforeach
+        </select><br><br>
+
+        {{-- 内容 --}}
+        <label>お問い合わせ内容 ※</label><br>
+        <textarea name="detail" rows="5" cols="40" placeholder="お問い合わせ内容をご記入ください">{{ old('detail') }}</textarea>
+<br><br>
+
+        <button type="submit">確認画面</button>
+    </form>
+</div>
 @endsection
-
